bab67ef8aa8610aa6e31ce2b0a100addf6786e44
danielvazquez-utp <daniel.vazquez@utpuebla.edu.mx>
2024-10-23 18:36:59 -0600

    Componentes de graficación ECharts agregados

---
+++ src/components/echarts/BarsChart.jsx
@@ -0,0 +1,21 @@
+import ReactECharts from "echarts-for-react";
+export const BarsChart = () => {
+    const options = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar'
+            }
+        ]
+    };
+    return (
+        <ReactECharts option={ options } />
+    )
+}
